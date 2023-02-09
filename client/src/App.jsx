859e0b9e1872b93041a3e794abcd5c9899f90c62
Hamid <hp@qaap.io>
2023-02-10 00:48:50 +0330

    Init project using react, vite, pnpm.

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+     
+      <h1>Asiabari</h1>
+      <div className="card">
+       React Expert developer
+      </div>
+      <p className="read-the-docs">
+        For more information check my projects.
+        
+        </p>
+    </div>
+  )
+}
+
+export default App
