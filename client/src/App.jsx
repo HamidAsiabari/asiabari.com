31c999728db60e9061009cb4550595249a917a9e
Hamid <hp@qaap.io>
2023-02-10 14:07:40 +0330

    Add project section.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,10 @@ import './App.css';
 import ResponsiveAppBar from './components/appBar/appBar';
 import Stack from '@mui/material/Stack';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import prof from './assets/prof.jpg';
+import ProjectCard from './components/projects/project';
+import Container from '@mui/material/Container';
+
+import Box from '@mui/material/Box';
 
 const lightTheme = createTheme({
   palette: {
@@ -42,55 +45,100 @@ function App() {
    
 
 
-    <h1>Asiabari</h1>
-    <h1>Asiabari</h1>
-      <div className="card">
-       React Expert developer
+    <h1>My experiences</h1>
+      <div className="card expText">
+        <p >
+      My experience started with programming in Visual Basic.</p><p>
+       After that, I was programming on microcontrollers with Bascom language. </p><p>
+        C# was the next programming language that I used for student and personal projects.</p><p>
+         I started Java programming to write Android applications, but I got to know the web field and my interest in the web world made me become a PHP and front-end programmer professionally as my profession.
+         </p><p> I worked with frameworks like CI and WordPress content management system and I was working with PHP for about 5 years.
+           At the same time, I tried assembly and C++ Plus and programming Apache modules.</p><p>
+            I implemented a project with Go Backend language. I did a research project on gene ontology with R language.</p><p>
+Finally, after all these activities, I decided to focus on JavaScript with the Ricket framework and use all the knowledge and experience I had to create the best web and applications.
+      </p>
+      
       </div>
-      <p className="read-the-docs">
-        For more information check my projects.
-        
-        </p><h1>Asiabari</h1>
-    <h1>Asiabari</h1>
-      <div className="card">
-       React Expert developer
-      </div>
-      <p className="read-the-docs">
-        For more information check my projects.
-        
-        </p><h1>Asiabari</h1>
-    <h1>Asiabari</h1>
-      <div className="card">
-       React Expert developer
-      </div>
-      <p className="read-the-docs">
-        For more information check my projects.
-        
-        </p><h1>Asiabari</h1>
-    <h1>Asiabari</h1>
-      <div className="card">
-       React Expert developer
-      </div>
-      <p className="read-the-docs">
-        For more information check my projects.
-        
-        </p><h1>Asiabari</h1>
-    <h1>Asiabari</h1>
-      <div className="card">
-       React Expert developer
-      </div>
-      <p className="read-the-docs">
-        For more information check my projects.
-        
-        </p><h1>Asiabari</h1>
-    <h1>Asiabari</h1>
-      <div className="card">
-       React Expert developer
-      </div>
+
+
+
+
+
       <p className="read-the-docs">
         For more information check my projects.
         
         </p>
+        
+        
+
+        <div class="bodyshadow formbody">
+
+
+
+
+
+        <div className="form">
+        
+        <div className="form_title">
+          Contact<span> me</span>
+        </div>
+        <form className="form_items">
+          <div className="form_inputs">
+            <input type="text" required />
+            <label>Your Name</label>
+          </div>  
+          <div className="form_inputs">
+            <input type="text" required />
+            <label>Your email</label>
+          </div>
+          <div className="form_inputs">
+            <textarea col="100" required />
+          </div>
+         
+          <button className="form_button">Send message</button>
+        </form>
+        <div className="form_other">
+          <a href="#">forgot password?</a>
+          <a href="#">Join Now</a>
+        </div>
+      </div>
+
+
+
+
+
+          </div>
+
+
+
+          <Container maxWidth="xl">
+
+        <h1>Examples of completed projects</h1>
+        <Box
+          sx={{
+            display: 'grid',
+            gap: 1,
+            gridTemplateColumns: 'repeat(4, 1fr)',
+          }}  >
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+
+          <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+  
+      </Box>
+      </Container>
+
+
     </div>
     </ThemeProvider>
     </Stack>
